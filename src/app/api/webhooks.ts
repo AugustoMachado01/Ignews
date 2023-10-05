fe6f871e0c70f78095a26e20cd09b22755960a99
AugustoMachado01 <augustomachadochieno01@gmail.com>
2023-10-05 21:47:23 +0100

    SMS

--- src/app/api/webhooks.ts
+++ src/app/api/webhooks.ts
@@ -1,53 +1,28 @@
+import { headers } from "next/headers";
 import { stripe } from "@/services/pricing";
-import { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from "stream";
 import Stripe from "stripe";
 
-async function buffer(readable: Readable) {
-  const chunks = [];
-
-  for await (const chunk of readable) {
-    chunks.push(typeof chunk === "string" ? Buffer.from(chunk) : chunk);
-  }
-  return Buffer.concat(chunks);
-}
-
-export const config = {
-  api: {
-    ReportBody: false,
-  },
-};
-
 const relevantEvents = new Set(["checkout.session.completed"]);
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === "POST") {
-    const buf = await buffer(req);
-    const secret = req.headers["stripe-signature"];
+async function handler(req: Request) {
+  const body = await req.text();
 
-    let event: Stripe.Event;
+  const signature = headers().get("Stripe-Signature") ?? "";
 
-    try {
-      event = stripe.webhooks.constructEvent(
-        buf,
-        secret!,
-        process.env.STRIPE_WEBHOOKS_SECRET
-      );
-    } catch (error: any) {
-      return res.status(400).send(`webhooks error: ${error.message}`);
-    }
+  let event: Stripe.Event;
 
-    const { type } = event;
-
-    if (relevantEvents.has(type)) {
-      console.log("evento recebido", event);
-    }
-
-    res?.json({ received: true });
-  } else {
-    res?.setHeader("Allow", "POST");
-    res?.status(405).end();
+  try {
+    event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOKS_SECRET
+    );
+  } catch (err: any) {
+    return new Response(
+      `webhooks Error: ${err instanceof Error ? err.message : "Unknown Error"}`,
+      { status: 404 }
+    );
   }
+  const session = event.data.object as Stripe.Checkout.Session;
 }
-
-export default handler;
